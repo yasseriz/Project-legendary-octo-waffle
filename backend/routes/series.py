8c6bf2cb092d0a34509ba9a363b42512dec87dd6
yasseriz <yasserizaheer@gmail.com>
2020-12-07 21:26:21 +0800

    Added DELETE capabilities

--- backend/routes/series.py
+++ backend/routes/series.py
@@ -1,53 +1,56 @@
-from fastapi import APIRouter, Body, Request
-from fastapi.encoders import jsonable_encoder
-from fastapi.templating import Jinja2Templates
-
-templates = Jinja2Templates(directory="./frontend/")
-
-from backend.database import (
-    addSeries,
-    getSeries,
-    deleteSeries,
-    updateSeries,
-)
-
 from backend.models.schema import (
     ErrorResponseModel,
     ResponseModel,
     DataSchema,
     updateDataSchema,
 )
+from backend.database import (
+    addSeries,
+    getSeries,
+    deleteSeries,
+    updateSeries,
+)
+from fastapi import APIRouter, Body, Request
+from fastapi.encoders import jsonable_encoder
+from fastapi.templating import Jinja2Templates
+
+templates = Jinja2Templates(directory="./frontend/")
+
 
 router = APIRouter()
 
+
 @router.post("/", response_description="Series data added into database")
-async def addSeriesData(request:Request):
+async def addSeriesData(request: Request):
     form = await request.form()
     seriesJSON = jsonable_encoder(form)
     newSeries = await addSeries(seriesJSON)
     series = await getSeries()
-    return templates.TemplateResponse("main.html", context={"request":request, "message":"Series added successfully", "series":series})
+    return templates.TemplateResponse("main.html", context={"request": request, "message": "Series added successfully", "series": series})
 
 
 @router.get("/", response_description="Series retrieved")
-async def getSeriesData(request:Request):
+async def getSeriesData(request: Request):
     series = await getSeries()
     if series:
-        return templates.TemplateResponse("main.html", context={"request":request, "message":"Series data retrieved successfully", "series":series})
-    return templates.TemplateResponse("main.html", context={"request":request, "message":"Empty list returned"})
+        return templates.TemplateResponse("main.html", context={"request": request, "message": "Series data retrieved successfully", "series": series})
+    return templates.TemplateResponse("main.html", context={"request": request, "message": "Empty list returned"})
 
 
-@router.delete("/{id}", response_description="Series data deleted from database")
-async def deleteSeriesData(id: str):
+@router.post("/{id}", response_description="Series data deleted from database")
+async def deleteSeriesData(id: str, request: Request):
     deletedSeries = await deleteSeries(id)
+    series = await getSeries()
     if deletedSeries:
-        return ResponseModel(
-            "Series with ID: {} removed".format(
-                id), "Series deleted successfully"
-        )
-    return ErrorResponseModel(
-        "An error occurred", 404, "Series with id {0} doesn't exist".format(id)
-    )
+        # return ResponseModel(
+        #     "Series with ID: {} removed".format(
+        #         id), "Series deleted successfully"
+        # )
+        return templates.TemplateResponse("main.html", context={"request": request, "message": "Series with ID: {} removed".format(id), "series": series})
+    # return ErrorResponseModel(
+    #     "An error occurred", 404, "Series with id {0} doesn't exist".format(id)
+    # )
+    return templates.TemplateResponse("main.html", context={"request": request, "message": "Series with id {0} doesn't exist".format(id), "series": series})
 
 
 @router.put("/{id}")
